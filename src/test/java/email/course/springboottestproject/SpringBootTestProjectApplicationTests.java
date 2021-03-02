d4ecae2f6c145ab8d92a1a1ababd1572be525dc1
Paul Shirmovski <pshirmovski@gmail.com>
2021-03-02 23:12:05 +0100

    Initial commit

---
+++ src/test/java/email/course/springboottestproject/SpringBootTestProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package email.course.springboottestproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootTestProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
