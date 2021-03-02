6a3edee5f994d6e5b657ebcdf31608aa2a15ea45
Paul Shirmovski <pshirmovski@gmail.com>
2021-03-03 00:05:23 +0100

    added mockito unit tests

---
+++ src/main/java/email/course/springboottestproject/ProductVerifier.java
@@ -0,0 +1,7 @@
+package email.course.springboottestproject;
+
+public class ProductVerifier {
+    public boolean isCurrentlyInStockOfCompetitor(String productName) {
+        throw new UnsupportedOperationException();
+    }
+}
---
+++ src/test/java/email/course/springboottestproject/PricingServiceTest.java
@@ -0,0 +1,30 @@
+package email.course.springboottestproject;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.math.BigDecimal;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class PricingServiceTest {
+
+    @Mock
+    private ProductVerifier mockedProductVerifier;
+
+
+
+    @Test
+    public void shouldReturnCheapPriceWhenProductIsInStockOfCompetitor() {
+        when(mockedProductVerifier.isCurrentlyInStockOfCompetitor("AirPods"))
+                .thenReturn(true); //Specify what boolean value to return
+
+        PricingService cut = new PricingService(mockedProductVerifier);
+
+        assertEquals(new BigDecimal("99.99"), cut.calculatePrice("AirPods"));
+    }
+}
